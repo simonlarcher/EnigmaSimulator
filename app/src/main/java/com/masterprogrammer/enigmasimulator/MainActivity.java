8fb8b8ce177c30d1a55c0f4b7cd873e99e171b8c
simonlarcher <simolarcher@gmail.com>
2018-03-07 13:37:22 +0100

    fgn

--- app/src/main/java/com/masterprogrammer/enigmasimulator/MainActivity.java
+++ app/src/main/java/com/masterprogrammer/enigmasimulator/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         gui = new Gui(this);
         core = new Core();
+        //boh
         allesInizialisieren();
         idsZuweisen();
         textViewsTextZuweisen();
